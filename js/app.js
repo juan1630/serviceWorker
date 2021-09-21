f3fd61ea3073c90d535daa21e79d1922bf962fe5
Juan Patron <josejuanpatron1630@gmail.com>
2021-09-20 22:00:24 -0500

    Se agregó el sync y el push

--- js/app.js
+++ js/app.js
@@ -6,6 +6,19 @@
 // una de las formas mas cortas de comprobar 
 if ( navigator.serviceWorker ) {
     // console.log('funcionan los SW');
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('/sw.js')
+    .then( regist => {
+        // setTimeout(() =>{
+        //     regist.sync.register('posteo-perros');
+        //     console.log("hizo el post de las fotos");
+        // }, 3000);
+
+        Notification.requestPermission()
+        .then( (resultado) => {
+            console.log(resultado)
+            regist.showNotification('Hola mundo');
+        });
+
+    });
     // esta funcion instala el sw y si ya está instalado no lo vuelve a instalar
 }
--- sw.js
+++ sw.js
@@ -2,13 +2,39 @@
 // Los service worker deben de tener el control total de la carpeta 
 
 
-self.addEventListener('fetch', (event) => {
+// self.addEventListener('fetch', (event) => {
 
-    const respFoto = fetch(event.request)
-    // refactor de codigo
-                    .then( resp =>  ( resp.ok ) ? resp : fetch('./img/throme.jpeg'));
+//     // const respFoto = fetch(event.request)
+//     // // refactor de codigo
+//     //                 .then( resp =>  ( resp.ok ) ? resp : fetch('./img/throme.jpeg'));
     
-    //  el respondWith necesita retornar una respuesta 
-    event.respondWith(   respFoto   )
+//     // //  el respondWith necesita retornar una respuesta 
+//     // event.respondWith(   respFoto   )
 
+
+// });
+
+
+
+// SYNC: ES IDEA PARA CUANDO SE PIERDE LA CONEXION Y SE RECUPERA LA CONEXXION A INTERNET
+
+// self.addEventListener('sync', (event) => {
+
+//     console.log("Tenemos conexion");
+//     console.log(event)
+     
+//     console.log(event.tag);
+
+// });
+
+
+
+// MANEJAR LAS PUSH NOTIFICATIONS :
+
+
+
+self.addEventListener('push', (event) => {
+    // console.log(event)
+
+    console.log("notificacion recibida");
 });
